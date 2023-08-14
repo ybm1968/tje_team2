a07669fe4daec199b730e6a659ac212b90d06ca4
ybm1968 <yby1969@naver.com>
2023-08-14 14:01:48 +0900

    파일이동

---
+++ JAVA MISSION/src/JAVA_MISSION_HHG/_24_27/Driver.java
@@ -0,0 +1,26 @@
+package JAVA_MISSION_HHG._24_27;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+
+		RemoteControl RcCarRc = new RcCar();
+		RcCarRc.turnOn();
+		RcCarRc.setSpeed(150);
+		RcCarRc.setMute(true);
+		RcCarRc.turnOff();
+		RemoteControl.changeBattery();
+		System.out.println();
+		
+		RemoteControl DroneRc = new Drone();
+		DroneRc.turnOn();
+		DroneRc.setSpeed(150);
+		DroneRc.setMute(false);
+		DroneRc.turnOff();
+		RemoteControl.changeBattery();
+		System.out.println();
+		
+	}
+}
+
+
---
+++ JAVA MISSION/src/JAVA_MISSION_HHG/_24_27/Drone.java
@@ -0,0 +1,36 @@
+package JAVA_MISSION_HHG._24_27;
+
+	public class Drone implements RemoteControl {
+
+		private int Speed;			
+        private String changeBattery;
+        
+		@Override
+		public void turnOn() {
+			System.out.println("Drone 전원을 켭니다.");
+		}
+
+		@Override
+		public void turnOff() {
+			System.out.println("Drone 전원을 끕니다.");
+		}
+
+		@Override
+		public void setSpeed(int Speed) {
+			
+			if( Speed > RemoteControl.MAX_SPEED ) {
+				this.Speed = RemoteControl.MAX_SPEED;
+			}
+			
+			else if( Speed < RemoteControl.MIN_SPEED ) {
+				this.Speed = RemoteControl.MIN_SPEED;
+			}
+			else {
+				this.Speed = Speed;
+			}
+			System.out.println("현재 Drone 속력 : " + this.Speed);
+		}
+		
+	}
+
+
